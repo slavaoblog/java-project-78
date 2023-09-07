3fcc84ff3fe8950859fde60931b71f81e8d9a2fc
Viacheslav Oblog <slavaoblog@ya.ru>
2023-09-07 12:27:14 +0300

    correct checkstyle

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -5,12 +5,12 @@ import java.util.function.Predicate;
 
 public class MapSchema extends BaseSchema {
     public void required() {
-        Predicate<Object> predicateRequired = x -> x instanceof Map<?,?>;
+        Predicate<Object> predicateRequired = x -> x instanceof Map<?, ?>;
         super.addPredicate(predicateRequired);
     }
 
     public void sizeof(int size) {
-        Predicate<Object> predicateSizeof = x -> x instanceof Map<?,?>
+        Predicate<Object> predicateSizeof = x -> x instanceof Map<?, ?>
                 && ((Map<?, ?>) x).size() == size;
         super.addPredicate(predicateSizeof);
     }
